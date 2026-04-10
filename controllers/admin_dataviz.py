86d557b21038b39f042bcd0fe98dcd56bcd3b42a
flaily <clueeng@gmail.com>
2026-04-10 23:30:12 +0200

    finish dataviz

--- controllers/admin_dataviz.py
+++ controllers/admin_dataviz.py
@@ -11,25 +11,99 @@ admin_dataviz = Blueprint('admin_dataviz', __name__,
 @admin_dataviz.route('/admin/dataviz/etat1')
 def show_type_article_stock():
     mycursor = get_db().cursor()
+
+    # wishlist / categorie
+    #
     sql = '''
-    
-           '''
-    # mycursor.execute(sql)
-    # datas_show = mycursor.fetchall()
-    # labels = [str(row['libelle']) for row in datas_show]
-    # values = [int(row['nbr_articles']) for row in datas_show]
+          SELECT type_meuble.id_type_meuble      AS id_type_article,
+                 type_meuble.libelle_type_meuble AS libelle,
+                 COUNT(liste_envie.meuble_id)    AS nbr_articles
+          FROM type_meuble
+                   LEFT JOIN meuble ON meuble.type_meuble_id = type_meuble.id_type_meuble
+                   LEFT JOIN liste_envie ON liste_envie.meuble_id = meuble.id_meuble
+          GROUP BY type_meuble.id_type_meuble, type_meuble.libelle_type_meuble
+          ORDER BY type_meuble.id_type_meuble
+          '''
+    mycursor.execute(sql)
+    get_db().commit()
+    types_articles_nb = mycursor.fetchall()
+    print(types_articles_nb)
 
-    # sql = '''
-    #         
-    #        '''
-    datas_show=[]
-    labels=[]
-    values=[]
+    labels = [ligne['libelle'] for ligne in types_articles_nb]
+    values = [ligne['nbr_articles'] for ligne in types_articles_nb]
+    print("LABELS WISHLIST")
+    print(labels)
 
-    return render_template('admin/dataviz/dataviz_etat_1.html'
-                           , datas_show=datas_show
-                           , labels=labels
-                           , values=values)
+    sql = '''
+          SELECT type_meuble.id_type_meuble,
+                 type_meuble.libelle_type_meuble            AS libelle,
+                 COALESCE(SUM(historique.consultations), 0) AS nbr_consultations
+          FROM type_meuble
+                   LEFT JOIN meuble ON meuble.type_meuble_id = type_meuble.id_type_meuble
+                   LEFT JOIN historique ON historique.id_meuble = meuble.id_meuble
+              AND MONTH (historique.date_update) = MONTH (NOW())
+              AND YEAR (historique.date_update) = YEAR (NOW())
+          GROUP BY type_meuble.id_type_meuble, type_meuble.libelle_type_meuble
+          ORDER BY type_meuble.id_type_meuble
+          '''
+    mycursor.execute(sql)
+    get_db().commit()
+    histo_par_categorie = mycursor.fetchall()
+    print(histo_par_categorie)
+
+    histo_values = [int(ligne['nbr_consultations']) for ligne in histo_par_categorie]
+    print("HISTO VALUES")
+    print(histo_values)
+
+    sql = "SELECT id_type_meuble, libelle_type_meuble FROM type_meuble ORDER BY id_type_meuble"
+    mycursor.execute(sql)
+    get_db().commit()
+    toutes_categories = mycursor.fetchall()
+    print(toutes_categories)
+
+    selected_cat = request.args.get('categorie', None)
+    detail_articles = []
+    detail_labels = []
+    detail_wishlist = []
+    detail_histo = []
+
+    if selected_cat:
+        sql = '''
+              SELECT meuble.id_meuble,
+                     meuble.nom_meuble,
+                     COUNT(DISTINCT liste_envie.utilisateur_id) AS nbr_wishlist,
+                      COALESCE(SUM(historique.consultations), 0) AS nbr_historique
+              FROM meuble
+                       LEFT JOIN liste_envie ON liste_envie.meuble_id = meuble.id_meuble
+                       LEFT JOIN historique ON historique.id_meuble = meuble.id_meuble
+              WHERE meuble.type_meuble_id = %s
+              GROUP BY meuble.id_meuble, meuble.nom_meuble
+              ORDER BY meuble.nom_meuble
+              '''
+        mycursor.execute(sql, (selected_cat,))
+        get_db().commit()
+        detail_articles = mycursor.fetchall()
+        print("DETAIL ARTICLES")
+        print(detail_articles)
+
+        detail_labels = [art['nom_meuble'] for art in detail_articles]
+        # int obligatoire pcq sinon ca fait un Decimal
+        detail_wishlist = [int(art['nbr_wishlist']) for art in detail_articles]
+        detail_histo    = [int(art['nbr_historique']) for art in detail_articles]
+
+    return render_template(
+        'admin/dataviz/dataviz_etat_1.html',
+        types_articles_nb=types_articles_nb,
+        labels=labels,
+        values=values,
+        histo_values=histo_values,
+        toutes_categories=toutes_categories,
+        selected_cat=selected_cat,
+        detail_articles=detail_articles,
+        detail_labels=detail_labels,
+        detail_wishlist=detail_wishlist,
+        detail_histo=detail_histo,
+    )
 
 
 # sujet 3 : adresses
